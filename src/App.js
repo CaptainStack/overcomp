d30d3f0b676ca6e8d34306cdeeb33dd0d4647919
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-01 14:56:53 -0800

    Migrate main files from old project.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import HeroButton from './components/HeroButton';
+import HeroSlot from './components/HeroSlot';
+import TeamStats from './components/TeamStats';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = ({state}) => {
+  let hero_buttons = state.heroes.map((hero) => 
+    <HeroButton key={hero.id} 
+                hero={hero} 
+                roster={state.selected_heroes}
+                clickAction={state.buttonClick(hero.id)} />);
+  let hero_slots = state.selected_heroes.map((selected_hero) => 
+    <HeroSlot key={selected_hero.id} 
+              hero={selected_hero} 
+              clickAction={state.slotClick(selected_hero.id)} />);
+  return (
+    <div className="App">
+      <h1>Overcomp</h1>
+      <div className='row'>{hero_slots} <TeamStats team={state.selected_heroes} /></div>
+      <h2>Select Your Heroes</h2>
+      <div className='row'>{hero_buttons}</div>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,287 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+let state = {
+  heroes: [{
+    id: 1,
+    name: "Genji",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "genji-portrait.png",
+    category: "offense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 2,
+    name: "McCree",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "mccree-portrait.png",
+    category: "offense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 3,
+    name: "Pharah",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "pharah-portrait.png",
+    category: "offense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 4,
+    name: "Reaper",
+    hitpoints: 250,
+    armor: 0,
+    shields: 0,
+    portrait: "reaper-portrait.png",
+    category: "offense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 5,
+    name: "Soldier:  76",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "soldier-76-portrait.png",
+    category: "offense",
+    healing: 1,
+    tank: 0
+  },
+  {
+    id: 6,
+    name: "Sombra",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "sombra-portrait.png",
+    category: "offense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 7,
+    name: "Tracer",
+    hitpoints: 150,
+    armor: 0,
+    shields: 0,
+    portrait: "tracer-portrait.png",
+    category: "offense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 8,
+    name: "Bastion",
+    hitpoints: 200,
+    armor: 100,
+    shields: 0,
+    portrait: "bastion-portrait.png",
+    category: "defense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 9,
+    name: "Hanzo",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "hanzo-portrait.png",
+    category: "defense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 10,
+    name: "Junkrat",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "junkrat-portrait.png",
+    category: "defense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 11,
+    name: "Mei",
+    hitpoints: 250,
+    armor: 0,
+    shields: 0,
+    portrait: "mei-portrait.png",
+    category: "defense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 12,
+    name: "Torbjörn",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "torbjorn-portrait.png",
+    category: "defense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 13,
+    name: "Widowmaker",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "widowmaker-portrait.png",
+    category: "defense",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 14,
+    name: "D.Va",
+    hitpoints: 400,
+    armor: 200,
+    shields: 0,
+    portrait: "d-va-portrait.png",
+    category: "tank",
+    healing: 0,
+    tank: 2
+  },
+  {
+    id: 15,
+    name: "Reinhardt",
+    hitpoints: 300,
+    armor: 200,
+    shields: 0,
+    portrait: "reinhardt-portrait.png",
+    category: "tank",
+    healing: 0,
+    tank: 2
+  },
+  {
+    id: 16,
+    name: "Roadhog",
+    hitpoints: 600,
+    armor: 0,
+    shields: 0,
+    portrait: "roadhog-portrait.png",
+    category: "tank",
+    healing: 0,
+    tank: 1
+  },
+  {
+    id: 17,
+    name: "Winston",
+    hitpoints: 400,
+    armor: 100,
+    shields: 0,
+    portrait: "winston-portrait.png",
+    category: "tank",
+    healing: 0,
+    tank: 1
+  },
+  {
+    id: 18,
+    name: "Zarya",
+    hitpoints: 200,
+    armor: 0,
+    shields: 200,
+    portrait: "zarya-portrait.png",
+    category: "tank",
+    healing: 0,
+    tank: 1
+  },
+  {
+    id: 19,
+    name: "Ana",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "ana-portrait.png",
+    category: "support",
+    healing: 1,
+    tank: 0
+  },
+  {
+    id: 20,
+    name: "Lúcio",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "lucio-portrait.png",
+    category: "support",
+    healing: 2,
+    tank: 0
+  },
+  {
+    id: 21,
+    name: "Mercy",
+    hitpoints: 200,
+    armor: 0,
+    shields: 0,
+    portrait: "mercy-portrait.png",
+    category: "support",
+    healing: 2,
+    tank: 0
+  },
+  {
+    id: 22,
+    name: "Symmetra",
+    hitpoints: 100,
+    armor: 0,
+    shields: 100,
+    portrait: "symmetra-portrait.png",
+    category: "support",
+    healing: 0,
+    tank: 0
+  },
+  {
+    id: 23,
+    name: "Zenyatta",
+    hitpoints: 50,
+    armor: 0,
+    shields: 150,
+    portrait: "zenyatta-portrait.png",
+    category: "support",
+    healing: 1,
+    tank: 0
+  }
+],
+  selected_heroes: [
+    {id: 24, name: "Empty", hitpoints: 0, armor: 0, shields: 0, healing: 0, tank: 0},
+    {id: 25, name: "Empty", hitpoints: 0, armor: 0, shields: 0, healing: 0, tank: 0},
+    {id: 26, name: "Empty", hitpoints: 0, armor: 0, shields: 0, healing: 0, tank: 0},
+    {id: 27, name: "Empty", hitpoints: 0, armor: 0, shields: 0, healing: 0, tank: 0},
+    {id: 28, name: "Empty", hitpoints: 0, armor: 0, shields: 0, healing: 0, tank: 0},
+    {id: 29, name: "Empty", hitpoints: 0, armor: 0, shields: 0, healing: 0, tank: 0}
+  ],
+  buttonClick: hero_id => e => {
+    let clicked_hero = state.heroes.find((hero) => hero.id === hero_id);
+    let selected_index = state.selected_heroes.findIndex((hero) => hero.name === clicked_hero.name);
+    if (selected_index < 0) {
+      let empty_index = state.selected_heroes.findIndex((hero) => hero.name === "Empty");
+      state.selected_heroes[empty_index] = clicked_hero;
+    } else {
+      state.selected_heroes[selected_index] = {id: selected_index + 24, name: "Empty", hitpoints: 0, armor: 0, shields: 0, healing: 0, tank: 0};
+    }
+    ReactDOM.render(<App state={state}/>, document.getElementById('root'));
+  },
+  slotClick: slot_id => e => {
+    let clicked_index = state.selected_heroes.findIndex((slot) => slot.id === slot_id);
+    if (state.selected_heroes[clicked_index].name !== "Empty") {
+      state.selected_heroes[clicked_index] = {id: clicked_index + 24, name: "Empty", hitpoints: 0, armor: 0, shields: 0, healing: 0, tank: 0};
+      ReactDOM.render(<App state={state}/>, document.getElementById('root'));
+    }
+  }
+};
+
+ReactDOM.render(<App state={state}/>, document.getElementById('root'));
