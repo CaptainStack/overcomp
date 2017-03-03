d006253a308917c98846476182bafba5c48f13ed
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-02 23:42:43 -0800

    Refactor pixels to ems.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ const App = ({state}) => {
       <img id='logo' src='logo.png' alt='logo'/>
       <TeamRoster selected_heroes={state.selected_heroes} clickAction={state.slotClick} />
       <TeamSelect all_heroes={state.heroes} selected_heroes={state.selected_heroes} buttonClick={state.buttonClick}/>
-      {/*<div className='row' id='hero-select'>
-        <HeroSet heroes={offense_heroes} selected_heroes={state.selected_heroes} category='Offense' clickAction={state.buttonClick}/>
-        <HeroSet heroes={defense_heroes} selected_heroes={state.selected_heroes} category='Defense' clickAction={state.buttonClick}/>
-        <HeroSet heroes={tank_heroes} selected_heroes={state.selected_heroes} category='Tank' clickAction={state.buttonClick}/>
-        <HeroSet heroes={support_heroes} selected_heroes={state.selected_heroes} category='Support' clickAction={state.buttonClick}/>
-      </div>*/}
       <footer>
         <div><a href='https://captainstack.github.io/public-stackhouse'><p>© 2017 by Andre Stackhouse</p></a></div>
         <div><a href='https://github.com/CaptainStack/public-stackhouse' target='_blank'><p>View source on GitHub</p></a></div>
