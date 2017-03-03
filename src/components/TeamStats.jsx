84311aa6f89a7f99cdef4f784f399a34b225513b
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-02 22:46:16 -0800

    Refactor TeamStat CSS.

--- src/components/TeamStats.jsx
+++ src/components/TeamStats.jsx
@@ -14,76 +14,51 @@ const TeamStats = ({team}) => {
   let healing_color;
   let tank_color;
 
-  if (healing === 0) { healing_message = 'No healer'; healing_color = 'indianred' }
-  else if (healing > 0 && healing < 4) { healing_message = 'Need more healing'; healing_color = 'indianred' }
-  else if (healing > 3 && healing < 6) { healing_message = 'Low healing'; healing_color = 'palegoldenrod' }
-  else { healing_color = 'darkseagreen'; healing_message = 'Good healing'}
+  if (healing === 0) { healing_message = 'No healer'; healing_color = 'indianred'; }
+  else if (healing > 0 && healing < 4) { healing_message = 'Need more healing'; healing_color = 'indianred'; }
+  else if (healing > 3 && healing < 6) { healing_message = 'Low healing'; healing_color = 'palegoldenrod'; }
+  else { healing_color = 'darkseagreen'; healing_message = 'Good healing'; }
 
-  if (tank === 0) { tank_message = 'No tank'; tank_color = 'indianred' }
-  else if (tank === 1) { tank_message = 'Need more tank'; tank_color = 'indianred' }
-  else if (tank < 3) { tank_message = 'Low tank'; tank_color = 'palegoldenrod' }
-  else { tank_color = 'darkseagreen'; tank_message = 'Good tank';}
+  if (tank === 0) { tank_message = 'No tank'; tank_color = 'indianred'; }
+  else if (tank === 1) { tank_message = 'Need more tank'; tank_color = 'indianred'; }
+  else if (tank < 3) { tank_message = 'Low tank'; tank_color = 'palegoldenrod'; }
+  else { tank_color = 'darkseagreen'; tank_message = 'Good tank'; }
 
   return (
-    <div id={'team-stats'}>
+    <div className={'TeamStats'}>
       <h2>Team Stats</h2>
       <table>
+        <colgroup>
+          <col className='left-column'></col>
+          <col className='center-column'></col>
+          <col className='right-column'></col>
+        </colgroup>
         <tbody>
           <tr>
-            <td style={{width: '100px', textAlign: 'right'}}>HP/Armor/Shields:</td>
-            <td style={{width: '50%'}}>
-              <div style={{display: 'inline-block', 
-                           backgroundColor: '#C8C7CF',
-                           transition: '0.5s',
-                           width: hitpoints / 3000 * 100  + '%', 
-                           height: '20px'}}></div>
-              <div style={{display: 'inline-block', 
-                           backgroundColor: '#C6A544',
-                           transition: '0.5s',
-                           width: armor / 3000 * 100  + '%', 
-                           height: '20px'}}></div>
-              <div style={{display: 'inline-block', 
-                           backgroundColor: '#2894B1',
-                           transition: '0.5s',
-                           width: shields / 3000 * 100  + '%', 
-                           height: '20px'}}></div>
+            <td>HP/Armor/Shields:</td>
+            <td>
+              <div className='stat-bar' style={{backgroundColor: '#C8C7CF', width: hitpoints / 3000 * 100  + '%'}}></div>
+              <div className='stat-bar' style={{backgroundColor: '#C6A544', width: armor / 3000 * 100  + '%'}}></div>
+              <div className='stat-bar' style={{backgroundColor: '#2894B1', width: shields / 3000 * 100  + '%'}}></div>
             </td>
-            <td style={{width: '150px', textAlign: 'left'}}>
-              {hitpoints + '/' + armor + '/' + shields}
-            </td>
+            <td>{hitpoints + '/' + armor + '/' + shields}</td>
           </tr>
           <tr>
-            <td style={{width: '100px', textAlign: 'right'}}>Barriers:</td>
-            <td style={{width: '50%'}}>
-              <div style={{display: 'inline-block', 
-                           backgroundColor: '#C8C7CF',
-                           transition: '0.5s',
-                           width: barrier / 6025 * 100 + '%', 
-                           height: '20px'}}></div>
-            </td>
-            <td style={{width: '150px', textAlign: 'left'}}>
-              {barrier}
-            </td>
+            <td>Barriers:</td>
+            <td><div className='stat-bar' style={{backgroundColor: '#C8C7CF', width: barrier / 6025 * 100 + '%'}}></div></td>
+            <td>{barrier}</td>
           </tr>
           <tr>
-            <td style={{width: '100px', textAlign: 'right'}}>Healing:</td>
-            <td><div style={{display: 'inline-block', 
-                             backgroundColor: healing_color, 
-                             transition: '0.5s',
-                             width: healing / 15 * 100  + '%', 
-                             height: '20px'}}></div></td>
-            <td style={{width: '150px', textAlign: 'left'}}>{healing_message}</td>
+            <td>Healing:</td>
+            <td><div className='stat-bar' style={{backgroundColor: healing_color, width: healing / 15 * 100  + '%'}}></div></td>
+            <td>{healing_message}</td>
           </tr>
           <tr>
-            <td style={{width: '100px', textAlign: 'right'}}>Tank:</td>
-            <td><div style={{display: 'inline-block', 
-                            backgroundColor: tank_color,
-                            transition: '0.5s',
-                            width: tank / 8 * 100  + '%', 
-                            height: '20px'}}></div></td>
-            <td style={{width: '150px', textAlign: 'left'}}>{tank_message}</td>
+            <td>Tank:</td>
+            <td><div className='stat-bar' style={{backgroundColor: tank_color, width: tank / 8 * 100  + '%'}}></div></td>
+            <td>{tank_message}</td>
           </tr>
-          </tbody>
+        </tbody>
       </table>
     </div>
   );
