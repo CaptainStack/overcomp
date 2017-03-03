9004c6a83d3faec08df0a0dcba3e75e6426fed20
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-02 17:04:32 -0800

    Add footer.

--- src/App.js
+++ src/App.js
@@ -24,6 +24,10 @@ const App = ({state}) => {
         <HeroSet heroes={tank_heroes} selected_heroes={state.selected_heroes} category='Tank' clickAction={state.buttonClick}/>
         <HeroSet heroes={support_heroes} selected_heroes={state.selected_heroes} category='Support' clickAction={state.buttonClick}/>
       </div>
+      <footer>
+        <div><a href='https://captainstack.github.io/public-stackhouse'><p>© 2017 by Andre Stackhouse</p></a></div>
+        <div><a href='https://github.com/CaptainStack/public-stackhouse' target='_blank'><p>View source on GitHub</p></a></div>
+      </footer>
     </div>
   );
 };
