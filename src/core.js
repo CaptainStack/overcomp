d0358470bf4d2a9fbe91900389c85d34bb54eb63
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-15 01:43:40 -0700

    Refactor into empty_hero.

--- src/core.js
+++ src/core.js
@@ -1,10 +1,5 @@
-export const INITIAL_STATE = [
-    {id: 25, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0},
-    {id: 26, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0},
-    {id: 27, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0},
-    {id: 28, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0},
-    {id: 29, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0},
-    {id: 30, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0}];
+let empty_hero = id => { return {id: id, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0} }
+export const INITIAL_STATE = [empty_hero(25), empty_hero(26), empty_hero(27), empty_hero(28), empty_hero(29), empty_hero(30)];
 
 export const add_hero_to_roster = (state, hero) => {
   let empty_index = state.findIndex((roster_hero) => roster_hero.name === "Empty");
@@ -19,7 +14,7 @@ export const add_hero_to_roster = (state, hero) => {
 export const remove_hero_from_roster = (state, hero) => {
   let hero_index = state.findIndex((roster_hero) => roster_hero === hero);
   if (hero_index !== -1) {
-    state[hero_index] = {id: hero_index + 25, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0};
+    state[hero_index] = empty_hero(hero_index + 25);
     return state;
   } else {
     return state;
