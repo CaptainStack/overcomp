641a42220c61ee4142ba456e0bbee5eafe86f0ae
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-22 17:49:31 -0700

    Integrate hover info events and component into app.

--- src/events.js
+++ src/events.js
@@ -16,3 +16,15 @@ export const slotClick = slot_id => e => {
     store.dispatch({ type: 'REMOVE_HERO_FROM_ROSTER', hero: roster.get(clicked_index) });
   }
 }
+
+export const heroHover = hero => e => {
+  store.dispatch({ type: 'ADD_HOVER_HERO', hero: hero });
+}
+
+export const hoverExit = hero => e => {
+  store.dispatch({ type: 'REMOVE_HOVER_HERO' });
+}
+
+export const trackMouse = e => {
+  store.dispatch({ type: 'UPDATE_MOUSE_POSITION', event: e });
+}
