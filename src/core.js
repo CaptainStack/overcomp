0b0ff06215977fd50628148a27f2a7b91bf128ba
Andre Stackhouse <CaptainStack@outlook.com>
2017-03-22 17:48:09 -0700

    Update state to manage mouse position & hover hero

--- src/core.js
+++ src/core.js
@@ -1,7 +1,11 @@
 import { List, Map } from 'immutable';
 
 let empty_hero = id => { return {id: id, name: "Empty", hitpoints: 0, armor: 0, shields: 0, barrier: 0, healing: 0, tank: 0} }
-export const INITIAL_STATE = Map({team: List([empty_hero(25), empty_hero(26), empty_hero(27), empty_hero(28), empty_hero(29), empty_hero(30)])});
+export const INITIAL_STATE = Map({
+  team: List([empty_hero(25), empty_hero(26), empty_hero(27), empty_hero(28), empty_hero(29), empty_hero(30)]),
+  hover_hero: null,
+  mouse: Map({x: null, y: null})
+});
 
 export const add_hero_to_roster = (state, hero) => {
   let empty_index = state.get('team').findIndex((roster_hero) => roster_hero.name === "Empty");
@@ -19,4 +23,19 @@ export const remove_hero_from_roster = (state, hero) => {
   } else {
     return state;
   }
+}
+
+export const add_hover_hero = (state, hero) => {
+  return state.set('hover_hero', hero);
+}
+
+export const remove_hover_hero = (state) => {
+  return state.set('hover_hero', null);
+}
+
+export const update_mouse_position = (state, event) => {
+  let mouse = state.get('mouse');
+  let updateX = mouse.set('x', event.pageX);
+  let updateY = updateX.set('y', event.pageY);
+  return state.set('mouse', updateY);
 }
