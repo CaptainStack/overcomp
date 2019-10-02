fde7665d2f245125762f47fcfc53c7f2724276b5
Faith <milkgirl96@yahoo.com>
2019-10-01 20:42:38 -0500

    new updates

--- src/components/TeamRoster.jsx
+++ src/components/TeamRoster.jsx
@@ -4,14 +4,16 @@ import TeamStats from './TeamStats';
 import '../styles/TeamRoster.css';
 import { slotClick } from '../events';
 
-const TeamRoster = ({selected_heroes, clickAction}) => {
+ const TeamRoster = ({selected_heroes, clickAction}) => {
   let hero_slots = selected_heroes.map((selected_hero) => 
     <HeroSlot key={selected_hero.id} hero={selected_hero} clickAction={slotClick(selected_hero.id)} />);
   return (
-    <div id={'team-roster'} className='row'>
-      <div id='hero-slots'>{hero_slots}</div>
-      <TeamStats team={selected_heroes} />
-    </div>
+   
+      <div id={'team-roster'} className='row'>
+        <div id='hero-slots'>{hero_slots}</div> 
+       <TeamStats team={selected_heroes} />
+      </div>
+  
   );
 };
 
